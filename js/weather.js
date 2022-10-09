c9740a7ef8ca7e84bdb874b1fe6b5712050d8d08
feb-dain <feb.dain@gmail.com>
2022-10-09 20:40:01 +0900

    initial commit

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,9 @@
+
 function geoOn(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const API_KEY = config.apikey;
+    console.log(API_KEY);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url)
         .then((response) => response.json())
